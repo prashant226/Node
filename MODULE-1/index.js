afb8bb5997ca8ebdf7ba5910575a1ba93a05e881
prashant226 <prashantnaraniya2003@gmail.com>
2023-12-14 18:56:47 +0530

    commited

--- MODULE-1/index.js
+++ MODULE-1/index.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 // const hello = 'hello world';
 // console.log(hello);
 
-const textIn = fs.readFileSync('a.txt');
+const textIn = fs.readFileSync('a.txt', 'utf-8');
 console.log(textIn);
-const textOut = `this is waht we know about the avacado : ${textIn}.\nCreated on ${Data.now()}`;
+const textOut = `this is waht we know about the avacado : ${textIn}.\n Created on ${Data.now()}`;
 fs.writeFileSync('a.text', textOut);
 console.log('file has been written');
