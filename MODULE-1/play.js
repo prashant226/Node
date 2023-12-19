21ab75f83e9323ce1e8ca391f23db1983d0c2621
prashant226 <prashantnaraniya2003@gmail.com>
2023-12-19 19:56:25 +0530

    commited

---
+++ MODULE-1/play.js
@@ -0,0 +1,22 @@
+// var name = 'Max';
+// var age = 29;
+// var hobbies = true;
+const name = 'Max';
+let age = 29;
+const hobbies = true;
+
+
+const summarizeUser = (userName, userAge, userHobby) => {
+    return ('Name is ' + userName + ',age is ' + userAge + ', and the user has hobbies ' + userHobby);
+
+};
+
+//const addOne= a => a+1;
+// console.log(addOne(1))
+// console.log(name);
+console.log(summarizeUser(name, age, hobbies));
+
+const add = (a, b) => {
+    return a + b;
+
+};
