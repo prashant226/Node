67ba168ca1416799808f7a328d618e8d00d5edaf
prashant226 <prashantnaraniya2003@gmail.com>
2023-12-19 19:15:43 +0530

    commited

--- MODULE-1/first-app.js
+++ MODULE-1/first-app.js
@@ -1,4 +1,18 @@
 // console.log('hello from node.js');
+//fs===> FILE SYSTEM
 
 const fs = require('fs');
 fs.writeFileSync('hello.txt', 'hello from node.js');
+
+//JS in The Server
+
+//USERS==> Client(Browser)(JS in the Browser)===(request)===> SERVER==> response in HTML
+/**DataBase
+ * Auth
+ * input Validation
+ * business Logic
+ */
+
+/**Run Server : Create server and listen to incoming requests */
+/**Business Logic : Handle Requests , validate Input ,connect to Database */
+/**Return Responses (HTML, JSON) */
